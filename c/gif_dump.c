9868b10a54f4ed220a198f38e09bdebaee697f85
よや <yoya@awm.jp>
2010-10-01 15:13:24 +0900

    PNG/GIF ファイルのダンプコマンド (PNG は中途半端...)

---
+++ c/gif_dump.c
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <gif_lib.h>
+
+#define print_indent(num) if (num) { printf("%*s", num, " "); }
+
+typedef struct my_gif_buffer_ {
+    unsigned char *data;
+    unsigned long data_len;
+    unsigned long data_offset;
+} my_gif_buffer;
+
+int gif_data_read_func(GifFileType* GifFile, GifByteType* buf, int count) {
+    my_gif_buffer *gif_buff = (my_gif_buffer *) GifFile->UserData;
+    if (gif_buff->data_offset + count <= gif_buff->data_len) {
+        memcpy(buf, gif_buff->data + gif_buff->data_offset, count);
+        gif_buff->data_offset += count;
+    } else {
+        fprintf(stderr, "! gif_buff->data_offset(%lu) + count(%d) <= gif_buff->data_len(%lu)\n",
+                gif_buff->data_offset, count, gif_buff->data_len);
+        return 0;
+    }
+    return count;
+}
+
+void print_colormap(ColorMapObject *ColorMap, int indent_num) {
+    int i, j;
+    int count;
+    if (ColorMap == NULL) {
+        print_indent(indent_num);
+        printf("No ColorMap\n");
+        return ;
+    }
+    count = ColorMap->ColorCount;
+    print_indent(indent_num);
+    printf("ColorMapObject:\n");
+    print_indent(indent_num);
+    printf("    ColorCount=%d  BitsPerPixel=%d\n",
+           count, ColorMap->BitsPerPixel);
+    print_indent(indent_num);
+    printf("    Colors:\n");
+    for (i = 0 ; i < count ; i+=8 ) {
+        print_indent(indent_num);
+        printf("        [%03d]", i);
+        for (j = 0 ; (j < 8) & ((i+j) < count) ; j++) {
+            printf(" #%02x%02x%02x",
+                   ColorMap->Colors[i+j].Red,
+                   ColorMap->Colors[i+j].Green,
+                   ColorMap->Colors[i+j].Blue);
+        }
+        printf("\n");
+    }
+}
+
+void print_image_desc(GifImageDesc ImageDesc) {
+    printf("ImageDesc\n");
+    printf("    (Left, Top)=(%d, %d)  (Width, Height)=(%d, %d)  Interlace=%d\n",
+           ImageDesc.Left, ImageDesc.Top,
+           ImageDesc.Width, ImageDesc.Height,
+           ImageDesc.Interlace);
+    print_colormap(ImageDesc.ColorMap, 4);
+}
+
+int main(int argc, char **argv) {
+    char *gif_filename;
+    struct stat filestat;
+    FILE *fp;
+    my_gif_buffer gif_buff;
+    int i;
+    int ImageCount;
+    GifFileType *GifFile;
+    if (argc != 2) {
+        char *program_filename = argv[0];
+        fprintf(stderr, "Usage: %s <gif_filename>\n", program_filename);
+        return EXIT_FAILURE;
+    }
+    /*
+     * file read
+     */
+    gif_filename = argv[1];
+
+    if(stat(gif_filename, &filestat)) {
+        perror("stat giffile");
+        return EXIT_FAILURE;
+    }
+    gif_buff.data_len = filestat.st_size;
+    gif_buff.data_offset = 0;
+    fp = fopen(gif_filename, "rb");
+    if (! fp) {
+        fprintf(stderr, "Can't open file(%s)\n", gif_filename);
+        return EXIT_FAILURE;
+    }
+    gif_buff.data = calloc(gif_buff.data_len, 1);
+    fread(gif_buff.data, 1, gif_buff.data_len, fp);
+    fclose(fp);
+    /*
+     * giflib
+     */
+    GifFile = DGifOpen(& gif_buff, gif_data_read_func);
+    if (GifFile == NULL) {
+        fprintf(stderr, "DGifOpen failed\n");
+        return EXIT_FAILURE;
+    }
+    if (DGifSlurp(GifFile) == GIF_ERROR) {
+        fprintf(stderr, "DGifSlurp failed\n");
+        return EXIT_FAILURE;
+    }
+    /*
+     * dump gif infomation
+     */
+    printf("Screen Infomation:\n");
+    printf("    (Width, Height)=(%d, %d)  ",
+           GifFile->SWidth, GifFile->SHeight);
+    printf("ColorResolution=%d  ", GifFile->SColorResolution);
+    printf("BackGroundColor=%d\n", GifFile->SBackGroundColor);
+    print_colormap(GifFile->SColorMap, 0);
+    ImageCount= GifFile->ImageCount;
+    printf("ImageCount=%d\n", ImageCount);
+    for (i=0 ; i < ImageCount ; i++) {
+        int ii, BlockCount;
+        printf("# Image No. %d\n", i);
+        struct SavedImage image = GifFile->SavedImages[i];
+        print_image_desc(image.ImageDesc);
+        BlockCount = image.ExtensionBlockCount;
+        printf("ExtensionBlockCount=%d\n", BlockCount);
+        for (ii=0; ii< BlockCount; ii++) {
+            int iii;
+            ExtensionBlock block = image.ExtensionBlocks[ii];
+            int Function = block.Function;
+            int ByteCount = block.ByteCount;
+            unsigned char *Bytes = (unsigned char*) block.Bytes;
+            printf("    Function=0x%x  ByteCount=%d  ",
+                   Function, ByteCount);
+            printf("Bytes:");
+            for ( iii=0 ; iii < ByteCount ; iii++ ) {
+                printf(" %02x", Bytes[iii]);
+            }
+            printf("\n");
+            switch(Function) {
+                int gcntl_flag;
+              case COMMENT_EXT_FUNC_CODE:  /* 0xfe */
+                printf("    Comment\n");
+                break;
+              case GRAPHICS_EXT_FUNC_CODE: /* 0xf9 */
+                printf("    Graphic Control\n");
+                gcntl_flag = block.Bytes[0];
+                printf("      DisposalMethod=%d ", (gcntl_flag >>2) & 0x7);
+                switch ((gcntl_flag >>2) & 0x7) {
+                  case 0:
+                    printf("(No disposal specified)");
+                    break;
+                  case 1:
+                    printf("(Do not dispose)");
+                    break;
+                  case 2:
+                    printf("(Restore to background color)");
+                    break;
+                  case 3:
+                    printf("(Restore to previous)");
+                    break;
+                  default:
+                    printf("(To be defines)");
+                    break;
+                }
+
+                
+                printf("\n");
+                printf("      UserInputFlag=%d  TransparentColorFlag=%d\n",
+                       (gcntl_flag>>1) & 0x1 , gcntl_flag & 0x01);
+                if (gcntl_flag & 0x02) { // user input flag
+                    int delay_time = 0x100 * block.Bytes[1] + block.Bytes[2];
+                    printf("      delay_time=%d\n", delay_time);
+                }
+                if (gcntl_flag & 0x01) { // transparent color flag
+                    int transparent_index = Bytes[3];
+                    ColorMapObject *ColorMap;
+                    if (image.ImageDesc.ColorMap) {
+                        ColorMap = image.ImageDesc.ColorMap;
+                    } else if (GifFile->SColorMap) {
+                        ColorMap = GifFile->SColorMap;
+                    } else {
+                        fprintf(stderr, "Not Found ColorMap\n");
+                        EGifCloseFile(GifFile);
+                        free(gif_buff.data);
+                        return  EXIT_FAILURE;
+                    }
+                    if (ColorMap->ColorCount < transparent_index) {
+                        fprintf(stderr, "ColorMap->ColorCount(%d) < transparent_index(%d)",
+                                ColorMap->ColorCount, transparent_index);
+                        EGifCloseFile(GifFile);
+                        free(gif_buff.data);
+                        return  EXIT_FAILURE;
+                    }
+                    GifColorType ct = ColorMap->Colors[transparent_index];
+                    printf("\tTransparent=%d (= #%02x%02x%02x)\n",
+                           transparent_index, ct.Red, ct.Green, ct.Blue);
+                }
+                break;
+              case PLAINTEXT_EXT_FUNC_CODE: /* 0x01 */
+                printf("    PlainText\n");
+                break;
+              case APPLICATION_EXT_FUNC_CODE: /* 0xff */
+                printf("    Application Block\n");
+                break;
+            }
+        }
+        if (image.RasterBits) {
+            GifImageDesc desc = image.ImageDesc;
+            int x, y;
+            ii = 0;
+            printf("RasterBits:\n");
+            for (y = 0 ; y < desc.Height ; y++) {
+                printf("    [%03d]", y + desc.Top);
+                for(x = 0 ; x < desc.Width ; x++) {
+                    printf(" %02x", image.RasterBits[ii] & 0xff);
+                    ii++;
+                }
+                printf("\n");
+            }
+        } else {
+            printf("No RasterBits\n");
+        }
+    }
+    DGifCloseFile(GifFile);
+    free(gif_buff.data);
+    return EXIT_SUCCESS;
+}
---
+++ c/png_dump.c
@@ -0,0 +1,218 @@
+/*
+  http://www.libpng.org/pub/png/libpng-1.2.5-manual.html
+  http://www5.cds.ne.jp/~kato/png/
+  http://www.amy.hi-ho.ne.jp/jbaba/png2dib.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <png.h>
+#include "bitstream.h"
+
+typedef struct my_png_buffer_ {
+    unsigned char *data;
+    unsigned long data_len;
+    unsigned long data_offset;
+} my_png_buffer;
+
+void png_data_read_func(png_structp png_ptr, png_bytep buf, png_size_t size){
+    my_png_buffer *png_buff = (my_png_buffer *)png_get_io_ptr(png_ptr);
+    if (png_buff->data_offset + size <= png_buff->data_len) {
+        memcpy(buf, png_buff->data + png_buff->data_offset, size);
+        png_buff->data_offset += size;
+    } else {
+        fprintf(stderr, "! png_buff->data_offset(%lu) + size(%d) <= png_buff->data_len(%lu)\n",
+                png_buff->data_offset, size, png_buff->data_len);
+        png_error(png_ptr,"png_read_read_func failed");
+    }
+}
+
+void png_data_read(png_structp png_ptr, my_png_buffer *png_buff) {
+    png_set_read_fn(png_ptr, (png_voidp) png_buff,
+                    (png_rw_ptr)png_data_read_func);
+}
+
+int main(int argc, char **argv) {
+    char *png_filename = NULL;
+    struct stat filestat;
+    FILE *fp = NULL;
+    my_png_buffer png_buff;
+    int is_png;
+    png_uint_32 png_width, png_height;
+    int bpp, color_type;
+    png_bytepp image_data;
+    png_uint_32 x, y;
+    png_color *palette = NULL;
+    int palette_num = 0;
+    png_bytep trans = NULL;
+    int num_trans;
+    png_color_16p trans_values = NULL;
+    int palette_dump_unit;
+    if (argc != 2) {
+        char *program_filename = argv[0];
+        fprintf(stderr, "Usage: %s <png_filename>\n", program_filename);
+        return EXIT_FAILURE;
+    }
+    png_filename = argv[1];
+//    printf("png_filename=%s\n", png_filename);
+
+    if(stat(png_filename, &filestat)) {
+        perror("stat pngfile");
+        return EXIT_FAILURE;
+    }
+    png_buff.data_len = filestat.st_size;
+    png_buff.data_offset = 0;
+//    printf("png_buff.data_len=%lu\n", png_buff.data_len);
+
+    fp = fopen(png_filename, "rb");
+    if (! fp) {
+        fprintf(stderr, "Can't open file(%s)\n", png_filename);
+        return EXIT_FAILURE;
+    }
+    png_buff.data = calloc(png_buff.data_len, 1);
+    fread(png_buff.data, 1, png_buff.data_len, fp);
+    fclose(fp);
+    is_png = png_check_sig((png_bytep)png_buff.data, 8);
+    if (! is_png) {
+        fprintf(stderr, "is not PNG!\n");
+        free(png_buff.data);
+        return EXIT_FAILURE;
+    }
+    png_structp png_ptr = png_create_read_struct
+        (PNG_LIBPNG_VER_STRING, NULL,NULL,NULL);
+    if (! png_ptr) {
+        fprintf(stderr, "can't create read_struct\n");
+        free(png_buff.data);
+        return EXIT_FAILURE;
+    }
+    png_infop png_info_ptr = png_create_info_struct(png_ptr);
+    if (! png_info_ptr) {
+        fprintf(stderr, "can't create info_struct\n");
+        png_destroy_read_struct (&png_ptr, NULL, NULL);
+        free(png_buff.data);
+        return EXIT_FAILURE;
+    }
+    png_data_read(png_ptr, &png_buff);
+    png_read_info(png_ptr, png_info_ptr);
+    png_get_IHDR(png_ptr, png_info_ptr,
+                 &png_width, &png_height, &bpp, &color_type,
+                 NULL, NULL, NULL);
+    printf("(width, height)=(%lu,%lu) bpp=%d",
+           png_width, png_height, bpp);
+    printf(" color_type=");
+    printf("%d", color_type);
+    switch(color_type) {
+      case PNG_COLOR_TYPE_GRAY:
+        printf("(GRAY)");
+        break;
+      case PNG_COLOR_TYPE_GRAY_ALPHA:
+        printf("(GRAY_ALPHA)");
+        break;
+      case PNG_COLOR_TYPE_RGB:
+        printf("(RGB)");
+        break;
+      case PNG_COLOR_TYPE_RGB_ALPHA:
+        printf("(RGB_ALPHA)");
+        if (png_get_valid(png_ptr, png_info_ptr, PNG_INFO_tRNS))
+            png_set_tRNS_to_alpha(png_ptr);
+        break;
+      case PNG_COLOR_TYPE_PALETTE:
+          printf("(PALETTE)");
+          png_get_PLTE(png_ptr, png_info_ptr, &palette, &palette_num);
+          printf(" palette_num=%d", palette_num);
+          if (png_get_tRNS(png_ptr, png_info_ptr, &trans, &num_trans,
+                           &trans_values)) {
+              png_get_bKGD(png_ptr, png_info_ptr, &trans_values);
+          }
+          printf("  num_trans=%d\n", num_trans);
+          break;
+      default:
+           printf("color_type(%d) not implement yet.\n",
+                  color_type);
+    }
+    printf("\n");
+    /*
+      image
+     */
+    image_data = (png_bytepp) malloc(png_height * sizeof(png_bytep));
+    for (y=0; y < png_height; y++) {
+        image_data[y] = (png_bytep) malloc(png_get_rowbytes(png_ptr, png_info_ptr));
+    }
+    png_read_image(png_ptr, image_data);
+    if (num_trans == 0) {
+        palette_dump_unit = 8;
+    } else {
+        palette_dump_unit = 4;
+    }
+    if (color_type == PNG_COLOR_TYPE_PALETTE) {
+        int i, j;
+        for (i = 0 ; i < palette_num ; i+=palette_dump_unit ) {
+            printf("[%03d]", i);
+            for (j = 0 ; (j < palette_dump_unit) && ((i+j) < palette_num) ; j++) {
+                if (i <= num_trans) {
+                    printf(" #%02x%02x%02x(%02x)",
+                           palette[i+j].red,
+                           palette[i+j].green,
+                           palette[i+j].blue,
+                           trans[i+j] & 0xff);
+                } else {
+                    printf(" #%02x%02x%02x",
+                           palette[i+j].red,
+                           palette[i+j].green,
+                           palette[i+j].blue);
+                }
+            }
+            printf("\n");
+        }
+        for (y=0; y < png_height; y++) {
+            unsigned char *linedata = image_data[y];
+            bitstream_t *bs = bitstream_open();
+            bitstream_input(bs, linedata, png_get_rowbytes(png_ptr, png_info_ptr));
+            printf("y=%lu: ", y);
+            for (x=0; x < png_width; x++) {
+                int colorindex = bitstream_getbits(bs, bpp);
+                printf("%02x  ", colorindex);
+            }
+            bitstream_close(bs);
+            printf("\n");
+        }
+    } else {
+        for (y=0; y < png_height; y++) {
+            printf("y=%lu: ", y);
+            for (x=0; x < png_width; x++) {
+                switch(color_type) {
+                case PNG_COLOR_TYPE_RGB:
+                    printf("%02x%02x%02x  ",
+                           image_data[y][3*x],
+                           image_data[y][3*x+1],
+                           image_data[y][3*x+2]);
+                    break;
+                case PNG_COLOR_TYPE_RGB_ALPHA:
+                    printf("%02x%02x%02x(%02x)  ",
+                           image_data[y][4*x],
+                           image_data[y][4*x+1],
+                           image_data[y][4*x+2],
+                           image_data[y][4*x+3]);
+
+                    break;
+                default:
+                    break;
+                }
+            }
+            printf("\n");
+        }
+    }
+    /*
+     * finish
+     */
+    for (y=0; y < png_height; y++) {
+        free(image_data[y]);
+    }
+    free(image_data);
+    png_destroy_read_struct(&png_ptr, &png_info_ptr, NULL);
+    free(png_buff.data);
+    return EXIT_SUCCESS;
+}
