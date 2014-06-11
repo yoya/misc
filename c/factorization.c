e4494f5c140523d813900ad1eaab9bfbaf4f6def
yoya <yoya@awm.jp>
2014-06-11 19:59:50 +0900

    素因数分解

---
+++ c/factorization.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int num, n, f;
+
+    for (num = 2 ; num < 150 ; num++) {
+        printf ("%d:", num);
+        n = num;
+        for (f = 2 ; f <= sqrt(n) ; f++) {
+            while ((n % f) == 0) {
+                printf (" %d", f);
+                n /= f;
+            }
+        }
+    if (n > 1) {
+        printf (" %d", n);
+    }
+    printf("\n");
+  }
+  return 0;
+}
