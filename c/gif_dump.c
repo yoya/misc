154f4db199c5d14fae926ba0e0ae5e34ed60e80b
yoya <yoya@g.awm.jp>
2016-10-27 20:01:23 +0900

    fixed typo

--- c/gif_dump.c
+++ c/gif_dump.c
@@ -184,14 +184,14 @@ int main(int argc, char **argv) {
                         ColorMap = GifFile->SColorMap;
                     } else {
                         fprintf(stderr, "Not Found ColorMap\n");
-                        DGitCloseFile(GifFile);
+                        DGifCloseFile(GifFile);
                         free(gif_buff.data);
                         return  EXIT_FAILURE;
                     }
                     if (ColorMap->ColorCount < transparent_index) {
                         fprintf(stderr, "ColorMap->ColorCount(%d) < transparent_index(%d)",
                                 ColorMap->ColorCount, transparent_index);
-                        DGitCloseFile(GifFile);
+                        DGifCloseFile(GifFile);
                         free(gif_buff.data);
                         return  EXIT_FAILURE;
                     }
