9be6e5eba3f7e31dac290ceca6729d5aa4553acc
yoya <yoya@g.awm.jp>
2016-11-13 23:55:43 +0900

    rgbRainbow

--- js/purecolor.js
+++ js/purecolor.js
@@ -2,12 +2,11 @@
 /*
  (c) 2016/11/13 - yoya@awm.jp
 */
-function cssColor(r,g,b) {
-    if (r instanceof Array) {
-	[r, g, b] = r;
-    }
+function cssColor(rgb) {
+    var [r, g, b] = rgb;
     return "rgb("+(r|0)+","+(g|0)+","+(b|0)+")";
 }
+
 function rgbMorph(rgb1, rgb2, ratio) {
     var [r1, g1, b1] = rgb1;
     var [r2, g2, b2] = rgb2;
@@ -33,55 +32,25 @@ var graphCtx  = graph.getContext("2d");
 graphCtx.globalCompositeOperation = "lighter";
 
 var ribbonGrad = ribbonCtx.createLinearGradient(0, 0, ribbon.width, 0);
+
+var rgbRainbow = [[255, 0, 0], [255, 255, 0], [0, 255, 0], [0, 255, 255], [0, 0, 255], [255, 0, 255], [255, 0, 0]];
+
 var x = 0;
-var color = null;
-
-var [rgb1, rgb2, rgb3] = [[255, 0, 0], [255, 255, 0], [0, 255, 0]];
-for (var i=0 ; i < 256; i++, x++) {
-    color = rgbMorph(rgb1, rgb2, i / 256);
-    ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
+for (var i = 0 ; i < 3 ; i++) {
+    var [rgb1, rgb2, rgb3] = rgbRainbow.slice(i*2, i*2+3);
+    drawLine(graphCtx, x, 1, x+256, 1, cssColor(rgb1));
+    drawLine(graphCtx, x, graph.height, x+256, 0, cssColor(rgb3));
+    for (var j=0 ; j < 256; j++, x++) {
+	var color = rgbMorph(rgb1, rgb2, j / 256);
+	ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
+    }
+    drawLine(graphCtx, x, 0, x+256, graph.height, cssColor(rgb1));
+    drawLine(graphCtx, x, 1, x+256, 1, cssColor(rgb3));
+    for (var j=0 ; j < 256; j++, x++) {
+	var color = rgbMorph(rgb2, rgb3, j/ 256);
+	ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
+    }
 }
-for (var i=0 ; i < 256; i++, x++) {
-    color = rgbMorph(rgb2, rgb3, i/ 256);
-    ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
-}
-drawLine(graphCtx, 0, 1, 256, 1, cssColor(rgb1));
-drawLine(graphCtx, 256, 0, 256*2, graph.height, cssColor(rgb1));
-drawLine(graphCtx, 0, graph.height, 256, 0, cssColor(rgb3));
-drawLine(graphCtx, 256, 1, 256*2, 1, cssColor(rgb3));
-
-//
-
-var [rgb1, rgb2, rgb3] = [[0, 255, 0], [0, 255, 255], [0, 0, 255]];
-for (var i=0 ; i < 256; i++, x++) {
-    color = rgbMorph(rgb1, rgb2, i / 256);
-    ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
-}
-for (var i=0 ; i < 256; i++, x++) {
-    color = rgbMorph(rgb2, rgb3, i/ 256);
-    ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
-}
-drawLine(graphCtx, 256*2, 1, 256*3, 1, cssColor(rgb1));
-drawLine(graphCtx, 256*3, 0, 256*4, graph.height, cssColor(rgb1));
-drawLine(graphCtx, 256*2, graph.height, 256*3, 0, cssColor(rgb3));
-drawLine(graphCtx, 256*3, 1, 256*4, 1, cssColor(rgb3));
-
-var [rgb1, rgb2, rgb3] = [[0, 0, 255], [255, 0, 255], [255, 0, 0]];
-for (var i=0 ; i < 256; i++, x++) {
-    color = rgbMorph(rgb1, rgb2, i / 256);
-    ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
-}
-for (var i=0 ; i < 256; i++, x++) {
-    color = rgbMorph(rgb2, rgb3, i/ 256);
-    ribbonGrad.addColorStop(x/ribbon.width, cssColor(color));
-}
-drawLine(graphCtx, 256*4, 1, 256*5, 1, cssColor(rgb1));
-drawLine(graphCtx, 256*5, 0, 256*6, graph.height, cssColor(rgb1));
-drawLine(graphCtx, 256*4, graph.height, 256*5, 0, cssColor(rgb3));
-drawLine(graphCtx, 256*5, 1, 256*6, 1, cssColor(rgb3));
-
-
-//
 
 ribbonCtx.beginPath();
 ribbonCtx.fillStyle = ribbonGrad;
