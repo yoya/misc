14686cfdb14e3859605710488a2ddd6a63a965de
yoya <yoya@g.awm.jp>
2016-11-19 02:21:17 +0900

    file stream input&output.

--- php/pngextract.php
+++ php/pngextract.php
@@ -23,28 +23,54 @@ $prefix = $options['p'];
 
 $pngSignature = "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A";
 
-function readUI32($data, $offset) { //BigEndian
-    $a = unpack("N", substr($data, $offset, 4));
+function searchText($fp, $needle) {
+    $len = strlen($needle);
+    $buff = fread($fp, $len);
+    if ($buff === false)  {
+        return false;
+    }
+    while (($buff !== $needle)) {
+        if (feof($fp)) {
+            return false;
+        }
+        $c = fread($fp, 1);
+        if ($c === false) {
+            return false;
+        }
+        $buff = substr($buff, 1) . $c;
+    }
+    return true;
+}
+
+function readUI32($str) { //BigEndian
+    $a = unpack("N", $str);
     return $a[1];
 }
 
 // main routine
 
-$data = file_get_contents($file);
-$dataLen = strlen($data);
+$fp = fopen($file, "rb");
+if ($fp === false) {
+    usage();
+    exit(1);
+}
 
 $offset = 0;
-for ($i = 0 ; true ; $i++) {
-    $startOffset = strpos($data, $pngSignature, $offset);
-    if ($startOffset === false) {
-        break;
-    }
-    $offset = $startOffset + strlen($pngSignature);
+for ($i = 0 ; searchText($fp, $pngSignature); $i++) {
+    $outputFilename = sprintf("%s%06d.png", $prefix, $i);
+    echo "$outputFilename\n";
+    $fp_out = fopen($outputFilename, "wb");
+    fwrite($fp_out, $pngSignature);
     $iendFound = false;
-    while (($offset + 8) < $dataLen) {
-        $len = readUI32($data, $offset);
-        $name = substr($data, $offset + 4 , 4);
-        $offset += 4 + 4 + $len + 4; // len + name + <payload> + crc
+    while (($len_name = fread($fp, 8)) !== false) {
+        fwrite($fp_out, $len_name);
+        $len = readUI32(substr($len_name, 0, 4));
+        $name = substr($len_name, 4, 4);
+        $payload_crc = fread($fp, $len + 4);
+        if ($payload_crc === false) {
+            break;
+        }
+        fwrite($fp_out, $payload_crc);
         if ($name === "IEND") {
             $iendFound = true;
             break;
@@ -54,13 +80,11 @@ for ($i = 0 ; true ; $i++) {
         echo "Incomplete PNG file found (offset:$offset)\n";
         break;
     }
-    $outputdata = substr($data, $startOffset, $offset - $startOffset);
-    $outputFilename = sprintf("%s%06d.png", $prefix, $i);
-    echo "$outputFilename (offset:$startOffset)\n";
-    file_put_contents($outputFilename, $outputdata);
-    $offset += strlen($outputdata);
+    fclose($fp_out);
 }
 
 echo "OK\n";
+
+fclose($fp);
 
 exit(0);
