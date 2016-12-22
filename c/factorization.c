90618343f0ed4efe11a09ddfa37f187248d48747
yoya <yoya@yoyaMBP2016late.local>
2016-12-22 20:05:36 +0900

    don't use sqrt (it's very heavy function)

--- c/factorization.c
+++ c/factorization.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(void) {
     int num, n, f;
     for (num = 2 ; num < 150 ; num++) {
         printf ("%d:", num);
         n = num;
-        for (f = 2 ; f <= sqrt(n) ; f++) {
+        for (f = 2 ; f*f <= n ; f++) {
             while ((n % f) == 0) {
                 printf (" %d", f);
                 n /= f;
