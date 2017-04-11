e7f2d7949e6a0e966fc34175538dc70c7ef022a0
Yoshihiro Yamazaki <yoya@awm.jp>
2017-04-11 16:16:10 +0900

    text shaper from PDF copy&paste

---
+++ php/pdftextshaper.php
@@ -0,0 +1,42 @@
+<?php
+
+$filename = $argv[1];
+
+$fp = fopen($filename, "rb");
+
+$text = "";
+$prev_c = null;
+
+$cr = false;
+
+$text .= "<<";
+
+while (($c = fgetc($fp)) !== false) {
+    if ($c === "\n") {
+        $text .= " ";
+    } else if ($c === " ") {
+        if ($prev_c === " ") {
+            // do nothing
+        } else {
+            if ($cr) {
+                $text .= PHP_EOL."<<".PHP_EOL.PHP_EOL.">>".PHP_EOL;
+                $cr = false;
+            } else {
+                $text .= " ";
+            }
+        }
+    } else if ($c === ".") {
+        $text .= $c;
+        $cr = true;
+    } else {
+        if ($cr) {
+            $text .= PHP_EOL."<<".PHP_EOL.PHP_EOL.">>".PHP_EOL;
+            $cr = false;
+        }
+        $cr = false;
+        $text .= $c;
+    }
+    $prev_c = $c;
+}
+
+echo $text;
