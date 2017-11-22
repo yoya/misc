b3ac44260fcba1276dac6153c8c01a7f80641095
Yoya <yoya@awm.jp>
2017-11-23 02:10:00 +0900

    calculate folding geometry.

--- js/threejs/folding3.js
+++ js/threejs/folding3.js
@@ -75,39 +75,11 @@ function updateTexture() {
     } else {
 	[planeRows, planeCols] = [3, 1];
     }
-    geometry = new THREE.PlaneGeometry( 2, 1, planeRows, planeCols );
-    geometry_back = new THREE.PlaneGeometry( 2, 1, planeRows, planeCols );
+    geometry = new THREE.PlaneGeometry( 2, 1.5, planeRows, planeCols );
+    geometry_back = new THREE.PlaneGeometry( 2, 1.5, planeRows, planeCols );
     console.log(geometry.vertices);
-    // folding geometry
-    if (foldingType === "fold2") {
-	geometry.vertices[0].x = geometry.vertices[3].x = -0.5;
-	geometry.vertices[0].z = geometry.vertices[3].z = 0.5;
-	geometry.vertices[2].x = geometry.vertices[5].x = 0.5;
-	geometry.vertices[2].z = geometry.vertices[5].z = 0.5;
-    } else if (foldingType === "fold3curl") {
-	geometry.vertices[0].x = geometry.vertices[4].x = -0.5;
-	geometry.vertices[0].z = geometry.vertices[4].z = 0.5;
-	geometry.vertices[3].x = geometry.vertices[7].x = 0.5;
-	geometry.vertices[3].z = geometry.vertices[7].z = 0.5;
-    } else {
-	geometry.vertices[0].x = geometry.vertices[4].x = -0.5;
-	geometry.vertices[0].z = geometry.vertices[4].z = 0.5;
-	geometry.vertices[3].x = geometry.vertices[7].x = 0.5;
-	geometry.vertices[3].z = geometry.vertices[7].z = -0.5;
-    }
-    // reverse geometry
-    if (reverseDirection === "horizontal") {
-	for (var i = 0, l = geometry.vertices.length ; i < l/2; i++) {
-	    geometry_back.vertices[      i] = geometry.vertices[l/2 - i - 1];
-	    geometry_back.vertices[l/2 + i] = geometry.vertices[l   - i - 1];
-	}
-    } else {
-	for (var i = 0, l = geometry.vertices.length ; i < l/2; i++) {
-	    geometry_back.vertices[i] = geometry.vertices[l/2 + i];
-	    geometry_back.vertices[l/2 + i] = geometry.vertices[i];
-	}
-    }
-    
+    foldingGeometry(foldingType, reverseDirection, 0.5);
+
     if (map === null) {
 	var texLoader = new THREE.TextureLoader();
 	texLoader.load('front.jpg', function(texture) { // onload
@@ -131,6 +103,111 @@ function updateTexture() {
 	mesh_back = new THREE.Mesh( geometry_back, material_back);
 	scene.add( mesh_back );
     }
+}
+
+// degree 0 => 1
+function foldingGeometry(foldingType, reverseDirection, degree) {
+    // folding geometry
+    if (foldingType === "fold2") {
+	/*
+	  0:(-1, 0.5) 1:(0, 0.5) 2:(1, 0.5)
+	  3:(-1,-0.5) 4:(0,-0.5) 5:(1,-0.5)
+	  const 1, 4
+	*/
+	var v_0 = new THREE.Vector3(-1, 0, 0 );
+	var v_2 = new THREE.Vector3( 1, 0, 0 );
+	var v_3 = new THREE.Vector3(-1, 0, 0 );
+	var v_5 = new THREE.Vector3( 1, 0, 0 );
+	console.log([v_0, v_2, v_3, v_5]);
+	var e_r = new THREE.Euler( 0, -Math.PI / 2 * degree, 0 );
+	var e_l = new THREE.Euler( 0, Math.PI / 2 * degree, 0 );
+	console.log("e_r, e_l:", e_r, e_l);
+	v_0.applyEuler(e_l);  v_2.applyEuler(e_r);
+	v_3.applyEuler(e_l);  v_5.applyEuler(e_r);
+	console.log("after euler:", [v_0, v_2, v_3, v_5]);
+	v_0.add(geometry.vertices[1]);
+	v_2.add(geometry.vertices[1]);
+	v_3.add(geometry.vertices[4]);
+	v_5.add(geometry.vertices[4]);
+	console.log("after add:", [v_0, v_2, v_3, v_5]);
+	geometry.vertices[0] = v_0;  geometry.vertices[2] = v_2;
+	geometry.vertices[3] = v_3;  geometry.vertices[5] = v_5;
+	console.log(geometry.vertices);
+    } else if (foldingType === "fold3curl") {
+	/*
+	  0:(-1, 0.5) 1:(-0.333, 0.5) 2:(0.333, 0.5) 3:(1,-0.5)
+	  4:(-1,-0.5) 5:(-0.333,-0.5) 6:(0.333,-0.5) 7:(1:-0.5)
+	  const 1, 2, 5, 6
+	*/
+	var v_0 = new THREE.Vector3(-(1/3), 0, 0 );
+	var v_3 = new THREE.Vector3( (1/3), 0, 0 );
+	var v_4 = new THREE.Vector3(-(1/3), 0, 0 );
+	var v_7 = new THREE.Vector3( (1/3), 0, 0 );
+	var e_r = new THREE.Euler( 0, -Math.PI / 2 * degree, 0 );
+	var e_l = new THREE.Euler( 0, Math.PI  * degree, 0 );
+	console.log("e_r, e_l:", e_r, e_l);
+	v_0.applyEuler(e_l);  v_3.applyEuler(e_r);
+	v_4.applyEuler(e_l);  v_7.applyEuler(e_r);
+	console.log("after euler:", [v_0, v_3, v_4, v_7]);
+	v_0.add(geometry.vertices[0]);
+	v_3.add(geometry.vertices[3]);
+	v_4.add(geometry.vertices[4]);
+	v_7.add(geometry.vertices[7]);
+	console.log("after add:", [v_0, v_3, v_4, v_7]);
+	geometry.vertices[0] = v_0;  geometry.vertices[3] = v_3;
+	geometry.vertices[4] = v_4;  geometry.vertices[7] = v_7;
+	console.log(geometry.vertices);
+    } else {
+	/*
+	  0:(-1, 0.5) 1:(-0.333, 0.5) 2:(0.333, 0.5) 3:(1,-0.5)
+	  4:(-1,-0.5) 5:(-0.333,-0.5) 6:(0.333,-0.5) 7:(1:-0.5)
+	  const 1, 2, 5, 6
+	*/
+	var v_0 = new THREE.Vector3(-(1/3), 0, 0 );
+	var v_3 = new THREE.Vector3( (1/3), 0, 0 );
+	var v_4 = new THREE.Vector3(-(1/3), 0, 0 );
+	var v_7 = new THREE.Vector3( (1/3), 0, 0 );
+	var e_r = new THREE.Euler( 0, Math.PI * degree, 0 );
+	var e_l = new THREE.Euler( 0, Math.PI * degree, 0 );
+	console.log("e_r, e_l:", e_r, e_l);
+	v_0.applyEuler(e_l);  v_3.applyEuler(e_r);
+	v_4.applyEuler(e_l);  v_7.applyEuler(e_r);
+	console.log("after euler:", [v_0, v_3, v_4, v_7]);
+	v_0.add(geometry.vertices[0]);
+	v_3.add(geometry.vertices[3]);
+	v_4.add(geometry.vertices[4]);
+	v_7.add(geometry.vertices[7]);
+	console.log("after add:", [v_0, v_3, v_4, v_7]);
+	geometry.vertices[0] = v_0;  geometry.vertices[3] = v_3;
+	geometry.vertices[4] = v_4;  geometry.vertices[7] = v_7;
+	console.log(geometry.vertices);
+    }
+    // reverse geometry
+    if (reverseDirection === "horizontal") {
+	for (var i = 0, l = geometry.vertices.length ; i < l/2; i++) {
+	    geometry_back.vertices[      i] = geometry.vertices[l/2 - i - 1];
+	    geometry_back.vertices[l/2 + i] = geometry.vertices[l   - i - 1];
+	}
+    } else {
+	for (var i = 0, l = geometry.vertices.length ; i < l/2; i++) {
+	    geometry_back.vertices[i] = geometry.vertices[l/2 + i];
+	    geometry_back.vertices[l/2 + i] = geometry.vertices[i];
+	}
+    }
+    geometry.verticesNeedUpdate = true;
+    geometry.elementsNeedUpdate = true;
+    geometry.morphTargetsNeedUpdate = true;
+    geometry.uvsNeedUpdate = true;
+    geometry.normalsNeedUpdate = true;
+    geometry.colorsNeedUpdate = true;
+    geometry.tangentsNeedUpdate = true;
+    geometry_back.verticesNeedUpdate = true;
+    geometry_back.elementsNeedUpdate = true;
+    geometry_back.morphTargetsNeedUpdate = true;
+    geometry_back.uvsNeedUpdate = true;
+    geometry_back.normalsNeedUpdate = true;
+    geometry_back.colorsNeedUpdate = true;
+    geometry_back.tangentsNeedUpdate = true;
 }
 
 function animate() {
