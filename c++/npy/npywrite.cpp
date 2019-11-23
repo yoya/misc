6a97474caf6dacb54fbac3e889803922f88f3b19
Yoshihiro Yamazaki <yoya@awm.jp>
2019-11-24 00:30:10 +0900

    npywrite specified data type by nh.valuetype

--- c++/npy/npywrite.cpp
+++ c++/npy/npywrite.cpp
@@ -18,7 +18,12 @@ void writeNPYheader(std::ofstream &fout,
   fout << NPY_FILE_SIG;
   // version: 1 (little-endian)
   fout.write(reinterpret_cast<const char *>("\1\0"), 2);
-  jsonss << "{'descr': '|u1', 'fortran_order': False, 'shape': (";
+  jsonss << "{'descr': '";
+  if ((nh.valuetype != "|u1") && (nh.valuetype != "<f4")) {
+    throw std::runtime_error("valuetype(descr):" + nh.valuetype + ", must be |u1 or <f4");;
+  }
+  jsonss << nh.valuetype;
+  jsonss << "', 'fortran_order': False, 'shape': (";
   for (auto itr = nh.shape.begin(); itr != nh.shape.end(); ++itr) {
     if (itr != nh.shape.begin()) {
       jsonss << ", ";
@@ -43,11 +48,22 @@ void writeNPYdata(std::ofstream &fout, const struct NPYheader_t &nh,
   if (imagedata == NULL) {
     throw std::runtime_error("imagedata == NULL");
   }
+  if ((nh.valuetype != "|u1") && (nh.valuetype != "<f4")) {
+    throw std::runtime_error("valuetype(descr):" + nh.valuetype + ", must be |u1 or <f4");
+  }
   int n = std::accumulate(nh.shape.begin(), nh.shape.end(), 1, std::multiplies<int>());
-  char fvalue_char[sizeof(T)];
-  for (int i = 0 ; i < n ; ++i) {
-    *(reinterpret_cast<T*>(fvalue_char)) = imagedata[i];
-    fout.write(fvalue_char, sizeof(T));
+  if (nh.valuetype == "|u1") {
+    for (int i = 0 ; i < n ; ++i) {
+      fout.put(imagedata[i]);
+    }
+  } else if (nh.valuetype == "<f4") {
+    char fvalue_char[4];
+    for (int i = 0 ; i < n ; ++i) {
+      *(reinterpret_cast<float*>(fvalue_char)) = imagedata[i];
+      fout.write(fvalue_char, 4);
+    }
+  } else {
+    throw std::runtime_error("unsupported type:"+nh.valuetype);
   }
 }
 
