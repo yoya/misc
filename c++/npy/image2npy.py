cae15898fe7be30a36c6d327711bfb9924c7bdff
Yoshihiro Yamazaki <yoya@awm.jp>
2021-09-17 23:49:25 +0900

    add sheban.

--- c++/npy/image2npy.py
+++ c++/npy/image2npy.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import sys
 from PIL import Image
 import numpy
@@ -5,4 +7,5 @@ import numpy
 im = Image.open(sys.argv[1])
 arr = numpy.asarray(im)
 #arr.tofile(sys.argv[2])
+#arr = arr.astype("float32");
 numpy.save(sys.argv[2], arr)
