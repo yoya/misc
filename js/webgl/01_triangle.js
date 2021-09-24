34612cb6558047462f57df85c9c31265d4a5b80d
Yoshihiro Yamazaki <yoya@awm.jp>
2021-09-24 13:07:42 +0900

    refine.

--- js/webgl/01_triangle.js
+++ js/webgl/01_triangle.js
@@ -1,57 +1,53 @@
 "use strict";
 
-var canvas = document.querySelector("#c");
-var gl = canvas.getContext("webgl");
+main();
 
-let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
-let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
- 
-let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-let program = createProgram(gl, vertexShader, fragmentShader);
+function main() {
+    var canvas = document.querySelector("#c");
+    var gl = canvas.getContext("webgl");
+    
+    let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+    let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+    
+    let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+    let program = createProgram(gl, vertexShader, fragmentShader);
+    
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-/*
-  
-*/
-
-var positionBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-// 三点の二次元頂点
-var positions = [
-  0, 0,
-  0, 0.5,
-  0.7, 0,
-];
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-// webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-gl.useProgram(program);
-
-gl.enableVertexAttribArray(positionAttributeLocation);
-
-// positionBufferをARRAY_BUFFERに結び付ける
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
- 
-// 属性にどうやってpositionBuffer（ARRAY_BUFFER)からデータを取り込むか。
-var size = 2;          // 呼び出すごとに2つの数値
-var type = gl.FLOAT;   // データは32ビットの数値
-var normalize = false; // データをnormalizeしない
-var stride = 0;        // シェーダーを呼び出すごとに進む距離
-                       // 0 = size * sizeof(type)
-var offset = 0;        // バッファーの頭から取り始める
-gl.vertexAttribPointer(
-    positionAttributeLocation, size, type, normalize, stride, offset)
-
-var primitiveType = gl.TRIANGLES;
-var offset = 0;
-var count = 3;
-gl.drawArrays(primitiveType, offset, count);
+    // 三点の二次元頂点
+    var positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    gl.useProgram(program);
+    
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    
+    var size = 2;
+    var type = gl.FLOAT;
+    var normalize = false;
+    var stride = 0;
+    var offset = 0;
+    gl.vertexAttribPointer(
+        positionAttributeLocation, size, type, normalize, stride, offset)
+    
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 3;
+    gl.drawArrays(primitiveType, offset, count);
+}
 
 function createShader(gl, type, source) {
     const shader = gl.createShader(type);
