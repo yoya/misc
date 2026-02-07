8e919d82e4fbde4c6a91172e38b4664e4d0382bb
Yoshihiro Yamazaki <yoya@awm.jp>
2026-02-07 11:11:54 +0900

    ffmpeg でなく ffprove を使ってダンプ

--- bash/ffidentify.sh
+++ bash/ffidentify.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+set -euo pipefail
+
 for f in "$@" ; do
+    echo -n "## filesize:"
     ls -s "$f"
     echo -n " "
-    ffmpeg -i "$f" >& /dev/stdout | grep -e Stream -e Duration
+    # ffmpeg -i "$f" 2>&1 | grep -e Stream -e Duration
+    ffprobe "$f" 2>&1 | grep -e Stream -e Duration
 done
